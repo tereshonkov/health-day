b94fc243ebfc672ca787f95902538f96542aa68d
tereshonkov <tereshonkov.dima@gmail.com>
2025-11-07 10:23:05 +0200

    feat: replace scheduleNotification with scheduleNotificationSafe for improved scheduling functionality

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -16,14 +16,33 @@ export async function registerForPush() {
     alert("Ви не надали доступ на повідомлення!");
     return;
   }
+  await Notifications.setNotificationChannelAsync("meds-reminders", {
+    name: "Нагадування",
+    importance: Notifications.AndroidImportance.HIGH,
+  });
+  return true;
 }
-
-export async function scheduleNotification(date: Date, title: string, body: string) {
-    await Notifications.scheduleNotificationAsync({
-      content: { title, body },
-      trigger: {
-        type: 'timestamp',
-        timestamp: date.getTime(), 
-      } as unknown as Notifications.DateTriggerInput,
-    });
-  }
+// export async function scheduleNotificationSafe(date: Date, title: string, body: string) {
+//     await Notifications.scheduleNotificationAsync({
+//       content: { title, body },
+//       trigger: {
+//         type: 'timestamp',
+//         timestamp: date.getTime(),
+//       } as unknown as Notifications.DateTriggerInput,
+//     });
+//   }
+export async function scheduleNotificationSafe(
+  date: Date,
+  title: string,
+  body: string
+) {
+  await Notifications.scheduleNotificationAsync({
+    content: { title, body },
+    trigger: {
+      type: Notifications.SchedulableTriggerInputTypes.CALENDAR,
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      repeats: true,
+    },
+  });
+}
