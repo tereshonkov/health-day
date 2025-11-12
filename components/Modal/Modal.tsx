b77e384162abd429c86f014cdc66b7cc70e3b4fc
tereshonkov <tereshonkov.dima@gmail.com>
2025-11-12 15:42:50 +0200

    feat: add reminderKey prop to ModalItem and update modal content

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,13 +1,16 @@
 import { Modal, Text, useColorScheme, View } from "react-native";
 import { darkTheme, lightTheme } from "../../styles/theme";
 
+type ReminderKey = "morning" | "lunch" | "dinner" | "night";
+
 interface ModalItemProps {
   isVisible: boolean;
   onClose: () => void;
   id?: string;
+  reminderKey?: ReminderKey;
 }
 
-export default function ModalItem({ isVisible, onClose, id }: ModalItemProps) {
+export default function ModalItem({ isVisible, onClose, id, reminderKey }: ModalItemProps) {
   const colorScheme = useColorScheme();
   const theme = colorScheme === "dark" ? darkTheme : lightTheme;
   return (
@@ -20,7 +23,11 @@ export default function ModalItem({ isVisible, onClose, id }: ModalItemProps) {
           console.log("Modal has been closed.");
         }}
       >
-        <Text style={theme.textXl}>Modal</Text>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+            <View style={[theme.container, {padding: 20}]}>
+                <Text style={[theme.textSm, theme.primary]}>Додай назву і кількість</Text>
+            </View>
+        </View>
       </Modal>
     </View>
   );
